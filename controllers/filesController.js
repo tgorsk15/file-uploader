d664c8cbea0c14ec2e91879a4e8213808782705c
tgorsk15 <tgbusiness463@gmail.com>
2024-09-15 10:42:03 -0500

    Create upload file view

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,5 +1,16 @@
 const db = require('../db/fileQueries');
+const folderDb = require('../db/folderQueries')
 
 exports.uploadFileGet = async (req, res) => {
+    const homeFolder = await folderDb.findFolderByName('Home');
+    const parentFolderId = req.params.folderId
+
     console.log('uploading file')
+
+    res.render("upload", {
+        title: 'Upload',
+        homeFolder: homeFolder,
+        homeChildren: homeFolder.children,
+        parentId: parentFolderId
+    })
 }
