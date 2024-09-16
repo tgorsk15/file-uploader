cf800a618510045f9c45b4c7933907107c18b9a0
tgorsk15 <tgbusiness463@gmail.com>
2024-09-16 09:07:10 -0500

    Develop DB function to store files into DB on upload

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -16,9 +16,24 @@ exports.uploadFileGet = async (req, res) => {
 }
 
 exports.uploadFilePost = async (req, res) => {
-    console.log('uploaded file:', req.file)
+    const parentFolderId = Number(req.params.folderId)
 
+    // insert into DB:
+    const newFile = await db.createNewFile(req.file.filename, parentFolderId)
+    console.log('here is new file', newFile)
+    // add file to parent folder's children:
+    const updateParent = await db.linkFileToFolder(newFile.id, parentFolderId)
+    console.log('folder after upload', updateParent)
+
+    // left off here:
     // will handle the logic of pushing the file to the
     // correct folder/user, while also adding the entries
     // to the DB
+
+    // redirect to home.ejs at the end to show new file
+    res.redirect(`/folder/library/${parentFolderId}`)
+}
+
+exports.deleteFile = async (req, res) => {
+    // work on this next
 }
--- db/fileQueries.js
+++ db/fileQueries.js
@@ -1,2 +1,42 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
+
+async function createNewFile(name, parentId) {
+    console.log('file info:', name, parentId)
+    const file = await prisma.file.create({
+        data: {
+            name: name,
+            folder: {
+                connect: {
+                    id: parentId
+                }
+            }
+        }
+    })
+    return file
+}
+
+async function linkFileToFolder(fileId, parentId) {
+    const addFile = await prisma.folder.update({
+        where: {
+            id: parentId
+        },
+        data: {
+            files: {
+                connect: {
+                    id: fileId
+                }
+            }
+        },
+        include: {
+            files: true
+        }
+    })
+    return addFile
+}
+
+
+module.exports = {
+    createNewFile,
+    linkFileToFolder
+}
